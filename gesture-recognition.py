47d7f4d7bfb06a3c125ad14922d10e20821c7fa8
Vipul Srivastav <vipulsrivastav92@gmail.com>
2018-09-18 20:45:02 -0400

    Add files via upload

---
+++ gesture-recognition.py
@@ -0,0 +1,120 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+@author: vipulsrivastav
+"""
+
+#importing the libraries
+import cv2 # version 3
+import numpy as np
+import math
+
+cv2.__version__ # gives version
+
+#initiating capture 
+cap = cv2.VideoCapture(0)
+
+while(True):
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+
+    # Region of interest
+    cv2.rectangle(frame, (300,300), (100,100), (0,255,0),0)
+    # Our operations on the frame come here
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    
+    crop_img = frame[100:300, 100:300]
+    
+    # grayscale
+    img_gray = cv2.cvtColor(crop_img, cv2.COLOR_BGR2GRAY)
+    
+    # soothing image using Gaussian Blur
+    value = (35, 35)
+    blurred = cv2.GaussianBlur(img_gray, value, 0)
+    
+    # thresholdin: Otsu's Binarization method
+    _, thresh1 = cv2.threshold(blurred, 127, 255,
+                               cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
+    
+    # show thresholded image
+    cv2.imshow('Thresholded', thresh1)
+    
+    image, contours, hierarchy = cv2.findContours(thresh1.copy(), \
+               cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+    
+    # find contour with max area
+    cnt = max(contours, key = lambda x: cv2.contourArea(x))
+
+    # create bounding rectangle around the contour (can skip below two lines)
+    x, y, w, h = cv2.boundingRect(cnt)
+    cv2.rectangle(crop_img, (x, y), (x+w, y+h), (0, 0, 255), 0)
+
+    # finding convex hull
+    hull = cv2.convexHull(cnt)
+
+    # drawing contours
+    drawing = np.zeros(crop_img.shape,np.uint8)
+    cv2.drawContours(drawing, [cnt], 0, (0, 255, 0), 0)
+    cv2.drawContours(drawing, [hull], 0,(0, 0, 255), 0)
+
+    # finding convex hull
+    hull = cv2.convexHull(cnt, returnPoints=False)
+
+    # finding convexity defects
+    defects = cv2.convexityDefects(cnt, hull)
+    count_defects = 0
+    cv2.drawContours(thresh1, contours, -1, (0, 255, 0), 3)
+
+    # applying Cosine Rule to find angle for all defects (between fingers)
+    # with angle > 90 degrees and ignore defects
+    for i in range(defects.shape[0]):
+        s,e,f,d = defects[i,0]
+
+        start = tuple(cnt[s][0])
+        end = tuple(cnt[e][0])
+        far = tuple(cnt[f][0])
+
+        # find length of all sides of triangle
+        a = math.sqrt((end[0] - start[0])**2 + (end[1] - start[1])**2)
+        b = math.sqrt((far[0] - start[0])**2 + (far[1] - start[1])**2)
+        c = math.sqrt((end[0] - far[0])**2 + (end[1] - far[1])**2)
+
+        # apply cosine rule here
+        angle = math.acos((b**2 + c**2 - a**2)/(2*b*c)) * 57
+
+        # ignore angles > 90 and highlight rest with red dots
+        if angle <= 90:
+            count_defects += 1
+            cv2.circle(crop_img, far, 1, [0,0,255], -1)
+        #dist = cv2.pointPolygonTest(cnt,far,True)
+
+        # draw a line from start to end i.e. the convex points (finger tips)
+        # (can skip this part)
+        cv2.line(crop_img,start, end, [0,255,0], 2)
+        #cv2.circle(crop_img,far,5,[0,0,255],-1)
+
+    # define actions required
+    if count_defects == 1:
+        cv2.putText(frame,"Number 1", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 2, 2)
+    elif count_defects == 2:
+        str = "This is Number 2"
+        cv2.putText(frame, str, (5, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, 2)
+    elif count_defects == 3:
+        cv2.putText(frame,"Number 3", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 2, 2)
+    elif count_defects == 4:
+        cv2.putText(frame,"Vipul Srivastava", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 2, 2)
+    else:
+        cv2.putText(frame,"Hello World!!!", (50, 50),\
+                    cv2.FONT_HERSHEY_SIMPLEX, 2, 2)
+
+    # show appropriate images in windows
+    cv2.imshow('Gesture', frame)
+    all_img = np.hstack((drawing, crop_img))
+    cv2.imshow('Contours', all_img)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# When everything done, release the capture
+cap.release()
+cv2.destroyAllWindows()
